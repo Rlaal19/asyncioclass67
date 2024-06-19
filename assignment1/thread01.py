3da4f51ad7e7f02f32ee57876e42b9b45ec0ac3b
Rlaal19 <n.prarichaya23@gmail.com>
2024-06-19 14:29:15 +0700

    success

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -10,9 +10,9 @@ def task():
     print(f'{ctime()} This is from another thread')
 
 # create a thread
-thead = Thread(target=task)
+thread = Thread(target=task)
 # run the thread
-thead.start()
+thread.start()
 # wait for the thread to finish
 print(f'{ctime()} waiting for the thread...')
-thead.join()
+thread.join()
